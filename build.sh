7ed77be89034b3b96f58d76c6e6ece50689886d5
kanutten <44906532+kanutten@users.noreply.github.com>
2025-02-07 19:40:53 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,11 +8,11 @@ set -ouex pipefail
 
 dnf -y install \
 
- "flameshot" \
- "nextcloud-client" \
- "nextcloud-client-nautilus" \
- "fuse" \
- "fuse-libs"
+ flameshot \
+ nextcloud-client
+ #"nextcloud-client-nautilus" \
+ #"fuse" \
+ #"fuse-libs"
  #sg3_utils
 #dnf config-manager --add-repo "https://download.docker.com/linux/centos/docker-ce.repo"
 #dnf config-manager --set-disabled docker-ce-stable
