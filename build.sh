946e0b3732509631e6181439aeb696c5775b0877
kanutten <44906532+kanutten@users.noreply.github.com>
2025-02-08 12:48:19 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,8 +15,7 @@ set -ouex pipefail
 #dnf config-manager --set-enabled crb
 #dnf -y install "https://dl.fedoraproject.org/pub/epel/epel-release-latest-$MAJOR_VERSION_NUMBER.noarch.rpm"
 dnf -y install fuse \
-fuse-libs \
-krusader
+fuse-libs
 #flameshot \
 #nextcloud-client-nautilus \ 
 #nextcloud-client
