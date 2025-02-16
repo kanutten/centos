ee418aadb02f4d83f3e102ca90c52a17f7e66afd
kanutten <44906532+kanutten@users.noreply.github.com>
2025-02-16 15:33:35 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,7 +13,8 @@ export MAJOR_VERSION_NUMBER
 #dnf -y --enablerepo docker-ce-stable install docker-ce \
 #dnf config-manager --set-enabled crb
 dnf -y install "https://dl.fedoraproject.org/pub/epel/epel-release-latest-$MAJOR_VERSION_NUMBER.noarch.rpm"
-dnf -y install "https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm"
+dnf -y install "https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-$MAJOR_VERSION_NUMBER.noarch.rpm"
+dnf -y install "https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$MAJOR_VERSION_NUMBER.noarch.rpm"
 dnf -y install fuse \
 flameshot \
 fuse-libs
