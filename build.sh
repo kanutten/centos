8826dc2eb3347798959ddc16742b12c4baffb582
kanutten <44906532+kanutten@users.noreply.github.com>
2025-02-16 15:44:38 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,11 +7,11 @@ set -ouex pipefail
 MAJOR_VERSION_NUMBER="$(sh -c '. /usr/lib/os-release ; echo $VERSION_ID')"
 export MAJOR_VERSION_NUMBER
 #set -xeuo pipefail
-#dnf config-manager --add-repo "registry.fedoraproject.org"
+dnf config-manager --add-repo "registry.fedoraproject.org"
 #dnf config-manager 
 #dnf config-manager --set-disabled docker-ce-stable
 #dnf -y --enablerepo docker-ce-stable install docker-ce \
-#dnf config-manager --set-enabled crb
+dnf config-manager --set-enabled crb
 dnf -y install "https://dl.fedoraproject.org/pub/epel/epel-release-latest-$MAJOR_VERSION_NUMBER.noarch.rpm"
 dnf -y install "https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-9.noarch.rpm"
 dnf -y install "https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-9.noarch.rpm"
