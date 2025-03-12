2de157a054c127c25b9ea15e6fb912b9a1ab64d2
kanutten <44906532+kanutten@users.noreply.github.com>
2025-03-12 22:06:06 +0100

    virtual

--- build.sh
+++ build.sh
@@ -6,45 +6,8 @@ set -ouex pipefail
 
 MAJOR_VERSION_NUMBER="$(sh -c '. /usr/lib/os-release ; echo $VERSION_ID')"
 export MAJOR_VERSION_NUMBER
-#set -xeuo pipefail
-#dnf config-manager --add-repo "registry.fedoraproject.org"
-#dnf config-manager 
-#dnf config-manager --set-disabled docker-ce-stable
-#dnf -y --enablerepo docker-ce-stable install docker-ce \
-#dnf config-manager --set-enabled crb
-#dnf -y install "https://dl.fedoraproject.org/pub/epel/epel-release-latest-$MAJOR_VERSION_NUMBER.noarch.rpm"
-#dnf -y install --nogpgcheck "https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm"
-#dnf -y install "https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-9.noarch.rpm"
-#dnf -y install "https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-9.noarch.rpm"
 dnf -y install fuse
-#flameshot \
-#fuse-libs
-#flameshot \
-#nextcloud-client-nautilus \ 
-#nextcloud-client
-
-
-
-
-
-
-#nextcloud-client-nautilus
-
- #nextcloud-client
- #"nextcloud-client-nautilus" \
- #"fuse" \
- #"fuse-libs"
- #sg3_utils
-#dnf config-manager --add-repo "https://download.docker.com/linux/centos/docker-ce.repo"
-#dnf config-manager --set-disabled docker-ce-stable
-#dnf -y --enablerepo docker-ce-stable install docker-ce \
-#   docker-ce-cli \
-#   containerd.io \
-#   docker-buildx-plugin \
-#   docker-compose-plugin && \
-#   systemctl enable docker 
-
- #dnf -y group install "Virtualization Hypervisor" 
+dnf -y install "Virtualization Hypervisor" 
  #dnf -y group install "Virtualization Tools"
 
 # ZFS Kernel Module
